7318ee7f3748812e3b9bf44643e84d7e4f2eca14
Arunrai0752 <realarunrai0752@gmail.com>
2025-06-06 18:37:28 +0530

    home page servises  complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,136 @@
 import React from "react";
+import { FaUserTie } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaChartLine } from "react-icons/fa";
+import { MdSmartphone } from "react-icons/md";
+import { FaHardHat } from "react-icons/fa";
+import { FaMicrochip } from "react-icons/fa";
+import { FaBuilding } from "react-icons/fa";
+import { FaPenNib } from "react-icons/fa";
 
 const Hero = () => {
   return (
     <>
-      <div className="bg-[url('h1_hero.jpg')] h-[100vh] w-[100vw]">
-        <h1>this is home</h1>
-      </div>
+      <main>
+        <section>
+          <div className="bg-[url('h1_hero.jpg')] h-[830px] w-[100%]  items-center ">
+            <div className="h-screen mx-auto  flex items-center relative bottom-[150px] pl-[200px]   ">
+              <h1 className="text-8xl font-extrabold text-gray-700 ">
+                Find the <br /> most exciting <br />
+                startup jobs{" "}
+              </h1>
+            </div>
+
+            <div className=" flex items-center text-zinc-500 text-2xl  relative bottom-[400px] left-[200px]">
+              <input
+                type="text"
+                placeholder="Job Tittle or keyword"
+                className="bg-gray-50 p-4 h-20 w-[360px] "
+              />
+
+              <select name="" id="" className="bg-gray-50 h-20 w-[360px] ">
+                <option value="">Location Bd</option>
+                <option value="">Location Pk</option>
+                <option value="">Location Us</option>
+              </select>
+              <button className="bg-pink-600 h-20 w-[190px] text-white ">
+                Find Job
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="h-[1500px] w-[100%] ">
+            <div className="flex grid text-center justify-center items-center h-[200px] py-[150px] mb-[150px] ">
+              <p className="text-2xl text-pink-600">
+                {" "}
+                FEATURED TOURS PACKAGES{" "}
+              </p>
+              <br />
+              <br />
+              <h1 className="text-6xl font-bold">Browse Top Categories</h1>
+            </div>
+
+            <div className=" flex  justify-center gap-10">
+              <div className="h-80 w-80 flex justify-center items-center text-center border-1 border-green-200  hover:text-pink-600  ">
+                <div>
+                  <FaUserTie className=" text-8xl relative left-10 text-blue-400   hover:text-pink-600" />
+                  <span className="text-2xl">Design & Creative</span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+              <div className="h-80 w-80 flex justify-center items-center text-center border-1 border-green-200  hover:text-pink-600">
+                <div>
+                  <FaLaptopCode className=" text-8xl relative left-18 text-blue-400  hover:text-pink-600 " />
+                  <span className="text-2xl">Design & Development </span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+              <div className="h-80 w-80 flex justify-center items-center text-center border-1 border-green-200  hover:text-pink-600">
+                <div>
+                  <FaChartLine className=" text-8xl relative left-12 text-blue-400  hover:text-pink-600" />
+                  <span className="text-2xl">Sales & Marketing</span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+              <div className="h-80 w-80 flex justify-center items-center text-center border-1 border-green-200  hover:text-pink-600">
+                <div>
+                  <MdSmartphone className=" text-8xl relative left-12 text-blue-400  hover:text-pink-600" />
+                  <span className="text-2xl">Mobile Application </span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className="flex  justify-center gap-10">
+              <div className="h-80 w-80 flex justify-center items-center text-center border-1 border-green-200  hover:text-pink-600">
+                <div>
+                  <FaHardHat className=" text-8xl relative left-6 text-blue-400  hover:text-pink-600 " />
+                  <span className="text-2xl">Construction</span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+              <div className="h-80 w-80 flex justify-center items-center text-center border-1 border-green-200  hover:text-pink-600">
+                <div>
+                  <FaMicrochip className=" text-8xl relative left-20 text-blue-400  hover:text-pink-600" />
+                  <span className="text-2xl">Information Technology </span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+              <div className="h-80 w-80 flex justify-center items-center text-center border-1 border-green-200  hover:text-pink-600">
+                <div>
+                  <FaBuilding className=" text-8xl relative left-2 text-blue-400  hover:text-pink-600" />
+                  <span className="text-2xl">Real Estate </span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+              <div className="h-80 w-80 flex grid justify-center items-center text-center border-1 border-green-200  hover:text-pink-600">
+                <div>
+                  <FaPenNib className=" text-8xl relative left-10 text-blue-400  hover:text-pink-600 " />
+                  <span className="text-2xl">Content Writer </span>
+                  <br />
+                  <span className="text-2xl text-pink-600">(658)</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className=" relative flex justify-center items-center bottom-[200px] ">
+
+            <button className="border-1 py-[25px] px-[80px] rounded-2xl border-blue-500 text-blue-500 text-2xl "> Browse All Sectors </button>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,28 +4,28 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <nav className=" flex justify-around align-content-center  m-2  ">
+      <nav className="  h-25 flex justify-around w-[100%] sticky top-0 px-30  bg-white ">
         <div>
-          <img src="/logo.png" className="" alt="Logo" />
+          <img src="/logo.png" className=" h-18 w-60 mt-5" alt="Logo" />
         </div>
-        <div className="list mx-[120px] flex items-center">
-          <ol className="list-unstyled  flex gap-10">
-            <NavLink>Homes</NavLink>
-            <NavLink>Finds a Jobs </NavLink>
+        <div className="list  flex items-center relative right-25 ">
+          <ol className="list-unstyled  flex gap-12">
+            <NavLink>Home</NavLink>
+            <NavLink>Find a Jobs </NavLink>
             <NavLink>About</NavLink>
             <NavLink>Page</NavLink>
             <NavLink>Contact</NavLink>
            
           </ol>
         </div>
-        <div className=" flex gap-3 items-center">
+        <div className="  flex gap-3 items-center  relative right-25">
           <div>
-            <button className=" bg-pink-700 py-4 px-12 text-white">
+            <button className=" bg-pink-600 py-5 px-15 text-white">
               Register
             </button>
           </div>
           <div>
-            <button className=" border-1 py-4 px-12 text-pink-700 hover:bg-pink-700 hover:text-white ">
+            <button className=" border-1 py-5 px-12 text-pink-700 hover:bg-pink-700 hover:text-white ">
               Login
             </button>
           </div>
