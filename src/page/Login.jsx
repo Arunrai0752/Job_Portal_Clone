387991a49cd0ebd266e55c2aa73adf0efba6cf41
Arunrai0752 <realarunrai0752@gmail.com>
2025-06-07 22:26:42 +0530

    Login And Register Complete

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -4,15 +4,14 @@ import { NavLink } from "react-router-dom";
 const Login = () => {
   return (
     <>
-      <div class="bg-[url(login.jpg)] bg-no-repeat  bg-cover bg-center h-[878.2px] w-screen  bg-gradient-to-r from-indigo-500/80 to-yellow-300/70   shadow-lg flex justify-center items-center ">
-       
-        <div className=" h-[550px] w-[550px] bg-gray-400  opacity-90 rounded-2xl ">
+      <div class="login bg-[url(login.jpg)] bg-no-repeat  bg-cover bg-center h-[878.2px] w-screen  bg-gradient-to-r from-indigo-500/80 to-yellow-300/70   shadow-lg flex justify-center items-center ">
+        <div className=" h-[650px] w-[650px] bg-gray-400  opacity-90 rounded-2xl ">
           <div className="h-[120px] text-center text-5xl bg-blue-600 font-bold p-[20px] text-white mb-[12px] pt rounded-1xl  ">
             <h1 className=" ">Log In</h1>
           </div>
 
-          <div className="relative top-[60px]  grid justify-around items-center ">
-            <div className=" h-[80px] relative p-5 w-550px flex  justify-around items-center  text-black rounded-b-2xl ">
+          <div className="relative top-[60px]   items-center ">
+            <div className=" h-[80px] relative p-5  flex  justify-around items-center  text-black rounded-b-2xl ">
               <span className=" text-[25px] float-start ">Email : </span>
               <input
                 type="email"
@@ -22,9 +21,8 @@ const Login = () => {
               />
             </div>
 
-
             <div className=" h-[80px] relative p-5 w-550px flex  justify-around items-center  text-black rounded-b-2xl  ">
-              <span className=" text-[20px] float-start ">Password :  </span>
+              <span className=" text-[20px] float-start ">Password : </span>
               <input
                 type="password"
                 id="name"
@@ -32,7 +30,6 @@ const Login = () => {
                 placeholder="Enter Your Password"
               />
             </div>
-
           </div>
 
           <div className="flex  text-2xl  justify-around items-center relative top-[80px]">
@@ -50,7 +47,16 @@ const Login = () => {
           </div>
 
           <div className="flex justify-center items-center relative top-[120px]">
-            <button className=" w-[290px] h-[60px] rounded text-2xl bg-amber-500 cursor-pointer hover:bg-amber-400 hover:h-[58px] hover:w-[280px ]">LogIn</button>
+            <button  className=" w-[290px] h-[60px] rounded text-2xl bg-amber-500 cursor-pointer hover:bg-amber-400 hover:h-[58px] hover:w-[280px ]">
+              LogIn
+            </button>
+          </div>
+
+          <div className="text-2xl flex justify-center items-center relative gap-3 top-[150px]">
+            <span> Don't have an Account </span> 
+            <NavLink to={"/register"} className="font-bold">
+              SignUp Now
+            </NavLink>
           </div>
         </div>
       </div>
--- src/page/Register.jsx
+++ src/page/Register.jsx
@@ -1,108 +1,110 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Register = () => {
   return (
-
     <>
-    <div className="bg-[url(Registerpage.jpg)] h-[878.2px] w-screen bg-cover bg-center flex">
+      <div className="overflow-hidden bg-[url(Registerpage.jpg)] h-[878.2px]  bg-cover bg-center flex">
+        <div className="h-[878.2px] w-[800px]"></div>
 
-        <div className="h-[878.2px] w-[800px]">
-er
-        </div>
-
-        <div  className="h-[878.2px] w-[1200px] flex justify-end items-center">
-
+        <div className="h-[878.2px] w-[1200px] flex justify-end items-center">
           <div className=" h-[878.2px] w-[800px] rounded-2xl bg-gray-500 opacity-80 ">
-
-
-
-            <div className="h-[120px] w-[800px] flex  bg-red-400 text-6xl justify-center items-center text-blue-700 font-bold "> <h1>Register</h1></div>
-
-
-
-            <div>
-
-                    <div className="h-[140px] ">
-
-                    <div className="text-center text-5xl flex justify-center items-center pt-5">
-                        <p>Name</p>
-                    </div>
-
-                    <div  className="text-center text-2xl flex py-5 " >
-                      <input type="Name" placeholder="First Name" className="bg-white h-[60px] w-[205px] ps-2 text-center"/>
-                      <span className="bg-white text-gray-400 p-2 text-4xl  w-[205px]">|</span>
-                      <input type="Name" placeholder="Middle Name" className="bg-white text-center"/>
-                      <span className="bg-white text-gray-400 p-2 text-4xl  w-[205px]">|</span>
-                      <input type="Name" placeholder="Last Name"className="bg-white text-center" />
-                    </div>
-
-                      
-                    </div>
-
-
-
-
-                    
-
-                    <div className="h-[140px] ">
-
-                    <div className="text-center text-5xl flex justify-center items-center pt-5 ">
-                        <p>Email</p>
-                    </div>
-
-                    <div  className="text-center text-2xl flex py-5 " >
-                      <input type="Name" placeholder="Enter Your Email" className="bg-white h-[60px] w-full ps-2 text-center"/>
-                     
-                    </div>
-
-                      
-                    </div>
-
-
-                    
-
-                    <div className="h-[140px] ">
-
-                    <div className="text-center text-5xl flex justify-center items-center pt-5">
-                        <p>Name</p>
-                    </div>
-
-                    <div  className="text-center text-2xl flex py-5 " >
-                      <input type="Name" placeholder="First Name" className="bg-white h-[60px] w-[205px] ps-2"/>
-                      <span className="bg-white text-gray-400 p-2 text-4xl  w-[205px]">|</span>
-                      <input type="Name" placeholder="Middle Name" className="bg-white"/>
-                      <span className="bg-white text-gray-400 p-2 text-4xl  w-[205px]">|</span>
-                      <input type="Name" placeholder="Last Name"className="bg-white" />
-                    </div>
-
-                      
-                    </div>
-
-
+            <div className="h-[120px] w-[800px] flex  bg-red-400 text-6xl justify-center items-center text-blue-700 font-bold ">
+              {" "}
+              <h1>Register</h1>
             </div>
 
+            <div>
+              <div className="h-[140px] ">
+                <div className="text-center text-4xl flex justify-center items-center pt-5">
+                  <p>Name</p>
+                </div>
 
+                <div className="text-center text-2xl flex py-5 ">
+                  <input
+                    type="Name"
+                    placeholder="First Name"
+                    className="bg-white h-[60px] w-[205px] ps-2 text-center outline-none"
+                  />
+                  <span className="bg-white text-gray-400 p-2 text-4xl  w-[205px]">
+                    |
+                  </span>
+                  <input
+                    type="Name"
+                    placeholder="Middle Name"
+                    className="bg-white text-center outline-none"
+                  />
+                  <span className="bg-white text-gray-400 p-2 text-4xl  w-[205px]">
+                    |
+                  </span>
+                  <input
+                    type="Name"
+                    placeholder="Last Name"
+                    className="bg-white text-centerd outline-none"
+                  />
+                </div>
+              </div>
 
+              <div className="h-[140px] ">
+                <div className="text-center text-4xl flex justify-center items-center pt-5 ">
+                  <p>Email</p>
+                </div>
 
+                <div className="text-center text-2xl flex py-5 ">
+                  <input
+                    type="email"
+                    placeholder="Enter Your Email"
+                    className="bg-white h-[60px] w-full ps-2 text-center outline-none"
+                  />
+                </div>
+              </div>
 
-            <div></div>
+              <div className="h-[140px] ">
+                <div className="text-center text-4xl flex justify-center items-center pt-5">
+                  <p>Create Password</p>
+                </div>
 
+                <div className="text-center text-2xl flex py-5 ">
+                  <input
+                    type="password"
+                    placeholder="Use Strong Password"
+                    className="bg-white h-[60px] w-full text-center ps-2 outline-none"
+                  />
+                </div>
+              </div>
 
+              <div className="h-[140px] ">
+                <div className="text-center text-4xl flex justify-center items-center pt-5">
+                  <p>Confirm Password</p>
+                </div>
 
+                <div className="text-center text-2xl flex py-5 ">
+                  <input
+                    type="password"
+                    placeholder=" Confirm Password"
+                    className="bg-white h-[60px] w-full text-center ps-2 outline-none"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-center items-center h-[150px] w-full">
+              <button className="h-[80px] text-5xl font-bold bg-pink-700 px-12 rounded-3xl hover:bg-blue-400">
+                SignUp
+              </button>
+            </div>
+
+            <div className="text-2xl flex justify-center gap-3 items-center">
+              <span> Already have an Account </span>
+              <NavLink to={"/login"} className="font-bold">
+                Login Now
+              </NavLink>
+            </div>
           </div>
-
-
         </div>
-
-
-
-
-
-    </div>
-    
-    
+      </div>
     </>
-  )
+  );
 };
 
 export default Register;
